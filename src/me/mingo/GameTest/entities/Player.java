e3cc90328b5484e52ffc62dbc554fa923ed83e0f
MingoMangoManden <victorschristensen47@gmail.com>
2022-06-19 19:11:20 +0200

    added velocity stuff

--- src/me/mingo/GameTest/entities/Player.java
+++ src/me/mingo/GameTest/entities/Player.java
@@ -2,77 +2,130 @@ package me.mingo.GameTest.entities;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
+import me.mingo.GameTest.GamePanel;
 import me.mingo.GameTest.Utils.Keyboard;
 
 public class Player extends Entity {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private int X, Y;
-	private int size;
-	private int speed;
+	public int x, y;
+	public int width, height; // block size
+	public  double speed;
 	
-	public Player(int startingX, int startingY, int size, int speed) {
-		this.X = startingX;
-		this.Y = startingY;
-		this.size = size;
+	public double[] velocity;
+	public double goofiness = 0.8;
+	
+	public Rectangle hitbox;
+	
+	public Player(int startingX, int startingY, int size, double speed) {
+		this.x = startingX;
+		this.y = startingY;
+		this.width = size;
+		this.height = size*2;
 		this.speed = speed;
+		this.velocity = new double[2];
+		this.hitbox = new Rectangle(x, y, width*GamePanel.tileSize, height*GamePanel.tileSize);
 	}
 
 	@Override
-	public void draw(Graphics2D g) {
-		g.setColor(Color.BLUE);
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.BLUE);
 		
-		g.fillRect(X, Y, size, size*2);
+		g2.fillRect(x, y, width*GamePanel.tileSize, height*GamePanel.tileSize);
+		hitbox = new Rectangle(x, y, width*GamePanel.tileSize, height*GamePanel.tileSize);
+		
+		// draw bounds
+		g2.setColor(Color.RED);
+		g2.draw(getBounds());
 	}
 
 	@Override
 	public void update() {
 		if (Keyboard.wPressed) {
-			Y -= speed;
+			velocity[1] += speed*-1;
 		}
 		if (Keyboard.aPressed) {
-			X -= speed;
+			velocity[0] += speed*-1;
 		}
 		if (Keyboard.sPressed) {
-			Y += speed;
+			velocity[1] += speed;
 		}
 		if (Keyboard.dPressed) {
-			X += speed;
+			velocity[0] += speed;
+		}
+		normalizeVelocity();
+	}
+	
+	private void normalizeVelocity() {
+		x += velocity[0];
+		y += velocity[1];
+		
+		
+		// X VELOCITY //
+		///////////////////////////////////////////////////////////////////
+		
+		// positive
+		if (velocity[0] > 0 && velocity[0] < 0.75) { // if between 0.0 and 0.75
+			velocity[0] = 0; // reset it
+		}
+		
+		// negative
+		if (velocity[0] < 0 && velocity[0] > -0.75) { // if between 0.0 and 0.75
+			velocity[0] = 0; // reset it
+		}
+		
+		///////////////////////////////////////////////////////////////////
+		
+		
+		// Y VELOCITY //
+		//////////////////////////////////////////////////////////////////////
+		
+		// positive
+		if (velocity[1] > 0 && velocity[1] < 0.75) { // if between 0.0 and 0.75
+			velocity[1] = 0; // reset it
+		}
+		
+		// negative
+		if (velocity[1] < 0 && velocity[1] > -0.75) { // if between 0.0 and 0.75
+			velocity[1] = 0; // reset it
+		}
+		
+		///////////////////////////////////////////////////////////////////
+		
+		if (velocity[0] != 0) {
+			velocity[0] *= goofiness;
+		}
+		if (velocity[1] != 0) {
+			velocity[1] *= goofiness;
 		}
 	}
 	
 	void jump() {
-		
-	}
-
-	@Override
-	public int getX() {
-		return X;
-	}
-
-	@Override
-	public int getY() {
-		return Y;
+		// TODO
 	}
 	
-	public int getSpeed() {
+	public int[] getMiddle() {
+		return new int[] {
+				(int) (width*0.5),
+				(int) (height*0.5)
+		};
+	}
+	
+	public Rectangle getBounds() {
+		return hitbox;
+	}
+
+	@Override
+	public double getSpeed() {
 		return speed;
 	}
 
 	@Override
-	public void setX(int newX) {
-		X = newX;
-	}
-
-	@Override
-	public void setY(int newY) {
-		Y = newY;
-	}
-	
-	public void setSpeed(int newSpeed) {
-		speed = newSpeed;
+	public double[] getVelocity() {
+		return velocity;
 	}
 
 }
